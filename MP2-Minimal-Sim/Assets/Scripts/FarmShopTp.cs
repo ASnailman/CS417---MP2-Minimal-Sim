6b38072a4e0049daf9e43fd563205730faaae8af
yyang2002 <yaweny2@illinois.edu>
2026-04-02 02:52:34 -0500

    Remove code used for xr interaction simulator debugging

--- MP2-Minimal-Sim/Assets/Scripts/FarmShopTp.cs
+++ MP2-Minimal-Sim/Assets/Scripts/FarmShopTp.cs
@@ -9,14 +9,7 @@ public class FarmShopTp : MonoBehaviour
     public void Teleport()
     {
         characterController.enabled = false; // Disable the character controller to prevent issues during teleportation
-
-        // Only for testing in XR Interactive Simulator
-        Vector3 targetPos = teleportTarget.position;
-        targetPos += Vector3.up * (characterController.height / 2f + characterController.skinWidth);
-        targetPos -= characterController.center;
-        characterController.transform.position = targetPos;
-
-        // characterController.transform.position = teleportTarget.position; // Teleport the player to the target position
+        characterController.transform.position = teleportTarget.position; // Teleport the player to the target position
         characterController.enabled = true; // Re-enable the character controller after teleportation
     }
 }
