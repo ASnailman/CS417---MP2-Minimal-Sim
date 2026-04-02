83c2ab7f7228ca1e69d1bd2b579238ae30605315
yyang2002 <yaweny2@illinois.edu>
2026-04-02 13:44:10 -0500

    Added cooldown timer on refill water, project version upgrade

--- MP2-Minimal-Sim/Assets/Scripts/RefillWater.cs
+++ MP2-Minimal-Sim/Assets/Scripts/RefillWater.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 
 public class RefillWater : MonoBehaviour
 {   
     public AudioSource refillWaterSound;
+    public AudioSource buttonActivatedSound;
     public HapticImpulsePlayer leftHaptic;
     public HapticImpulsePlayer rightHaptic;
+
+    public float cooldown = 5f;
+    private float remainingTime;
+    private bool isCoolingDown = false;
+    public Button refillButton;
+    public TextMeshProUGUI buttonText;
 
     public void refill()
     {
@@ -13,10 +22,40 @@ public class RefillWater : MonoBehaviour
             refillWaterSound.volume = 0.5f;
             refillWaterSound.Play();
         }
-        leftHaptic?.SendHapticImpulse(0.5f, 0.1f);
-        rightHaptic?.SendHapticImpulse(0.5f, 0.1f);
+        leftHaptic?.SendHapticImpulse(0.8f, 0.1f);
+        rightHaptic?.SendHapticImpulse(0.8f, 0.1f);
 
         float waterCapacity = (UpgradesManager.F_upgrades[1].level * 0.1f + 1) * 100;
         ResourceManager.Instance.water = waterCapacity;
+
+        if (refillButton != null)
+        {
+            isCoolingDown = true;
+            remainingTime = cooldown;
+
+            refillButton.interactable = false;
+            buttonText.text = "Wait " + Mathf.CeilToInt(remainingTime) + "s";
+        }
+    }
+
+    void Update()
+    {
+        if (!isCoolingDown) return;
+
+        remainingTime -= Time.deltaTime;
+        buttonText.text = "Wait " + Mathf.CeilToInt(remainingTime) + "s";
+
+        if (remainingTime <= 0f)
+        {
+            isCoolingDown = false;
+
+            if (refillWaterSound != null) {
+                buttonActivatedSound.volume = 1f;
+                buttonActivatedSound.Play();
+            }
+
+            refillButton.interactable = true;
+            buttonText.text = "Refill Water";
+        }
     }
 }
